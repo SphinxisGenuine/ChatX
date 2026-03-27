df9347de5570869d7b09c94790de6c3b11b9b84a
Sphinx <sphinxisgenuine@gmail.com>
2026-03-27 22:59:09 +0530

    Add Room creation in server side

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,46 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port:8080})
 
+interface msgs{
+    data?:string
+    type:string
+}
+
+interface room {
+    roomid:string,
+    User:WebSocket[]
+}
+
+const roomactive:room[]= [] 
+
+
+function roomcreation(a:WebSocket):string{
+const id = crypto.randomUUID()
+
+const room:room={
+    roomid:id,
+    User:[a]
+}
+roomactive.push(room)
+return id
+}
 
 wss.on("connection",(socket)=>{
 
 
     socket.on("message",(Raw)=>{
-        const msg=Raw.toString() 
-        wss.clients.forEach((client)=>{
-        if(client!==socket){
-            client.send(msg) 
-        }
-
-        })
+        const msg:msgs=JSON.parse(Raw.toString())
+        //@ts-ignore
+        if (msg.type==="create"){
+            const id=roomcreation(socket)
+            socket.send("Room id "+id)
+        }    
+        // wss.clients.forEach((client)=>{
+        // if(client!==socket){
+        //     client.send(msg) 
+        // }
+        // })
 
     })
     
