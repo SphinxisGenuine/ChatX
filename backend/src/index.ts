a043a3d0d97fc814d82fd74367b1128d5e7d925b
Sphinx <sphinxisgenuine@gmail.com>
2026-03-27 20:57:52 +0530

    add 1 Browser to Server Communication(one way communication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("initialize")
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port:8080})
+
+
+wss.on("connection",(socket)=>{
+
+
+    socket.send("hii")
+    socket.on("message",(Raw)=>{
+        const msg=Raw.toString()
+        socket.send(msg) 
+
+    })
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,48 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+const [socket,setSocket]=useState<WebSocket>()
+const [input,setinput]=useState("")
+  
 
+useEffect(()=>{
+
+
+const ws = new WebSocket("ws://localhost:8080")
+if(!ws){
+    return 
+  }
+  setSocket(ws)
+ws.onmessage=(msg)=>{
+
+  alert(msg.data)
+}
+
+},[])
+
+
+
+  function sendmessage(){
+    socket?.send(input)
+
+  }
   return (
-    <>
-     </>
+    <div className=' bg-neutral-500 flex h-screen justify-center items-center'>
+    <div className=' h-96 w-36'>
+<input onChange={inputhandler} value={input} className=' border-2 bg-neutral-100 outline-none border-neutral-300'></input>
+<button onClick={sendmessage} className='bg-red-700 px-2 border-2 rounded-lg'>Send</button>
+
+    </div>
+     </div>
   )
+
+
+  function inputhandler(e:any){
+const msg= e.target.value
+setinput(msg)  
+}
 }
 
 export default App
