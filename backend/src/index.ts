a9a7cb6e15e2a9ef7a79e9034ed2613aef5b84b2
Sphinx <sphinxisgenuine@gmail.com>
2026-03-29 19:05:37 +0530

    add Leaveroom function and also while joining and creating new remove the socket from the conneted rooms

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,18 @@ interface msgs{
 type id =string
 const rooms:Map<id,Set<WebSocket>> =new Map()
 const sockets:Map<WebSocket,id>=new Map()
-// todo to make sure client is present in only one room 
+// todo to make sure client is present in only one room
+
+function leaveCurrentroom(a:WebSocket){
+    if(sockets.has(a)){
+        const roomid=sockets.get(a)
+        Leaveroom(a,roomid!)
+    }
+
+}
+
 function roomcreation(a:WebSocket):string{
+    leaveCurrentroom(a)
 const id = crypto.randomUUID()
 rooms.set(id,new Set)
 rooms.get(id)?.add(a)
@@ -18,46 +28,44 @@ sockets.set(a,id)
 return id
 }
 
-wss.on("connection",(socket)=>{
+function Leaveroom(a:WebSocket,room:string){  
 
+    if(sockets.has(a)){
+            rooms.get(room!)?.delete(a)
+            sockets.delete(a)
+
+    }
+
+    if(rooms.get(room)?.size===0){
+        rooms.delete(room)
+    }
+}
+
+wss.on("connection",(socket)=>{
 
     socket.on("message",(Raw)=>{
         const msg:msgs=JSON.parse(Raw.toString())
-        //@ts-ignore
+        
         if (msg.type==="create"){
             const id=roomcreation(socket)
             socket.send("Room id "+id)
         }
         
         if (msg.type==="join"&&msg.data){
-
             if(rooms.has(msg.data)){
+                leaveCurrentroom(socket)
                 rooms.get(msg.data)?.add(socket)
                 sockets.set(socket,msg.data)
                 socket.send("Connected to room succesfully")
             }
-            
-            //  roomactive.map((t)=>{
-            //     if (t.roomid=== msg.data){
-            //         t.User.push(socket)
-                   
-            //         socket.send("You succefull joinned the room")
-                    
-            //     }
-            // })
-          
-             
-             
-
         }
 
         if (msg.type==="broadcast"&&msg.data){
             if (sockets.has(socket)){
                 const roomid=sockets.get(socket)
-                //@ts-ignore
-                const allembers=rooms.get(roomid)
+                const allembers=rooms.get(roomid!)
                     for (const client of allembers!){
-                        if (client!==socket){
+                        if (client!==socket&&(client.readyState===WebSocket.OPEN)){
                             client.send(msg.data)
                     }
                 }
@@ -70,7 +78,22 @@ wss.on("connection",(socket)=>{
 
 
         }
+        if(msg.type==="leave"){
+            const room= sockets.get(socket)
+            if (room){
+                Leaveroom(socket,room)
+                socket.send("Left the room")
+                socket.close()
+
+            }
+            
+
+        }
 
     })
+    socket.on("close",()=>{
+        leaveCurrentroom(socket)
+    })
+
     
 })
