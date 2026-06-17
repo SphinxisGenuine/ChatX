6288b92411a6553e405789552e297d4a19e8079f
Sphinx <sphinxisgenuine@gmail.com>
2026-06-17 14:32:57 +0000

    mv

---
+++ backend/src/db/db.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME
+})
+pool.on("connect", () => {
+  console.log("connection with database is succeful");
+})
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,128 +1,128 @@
 import WebSocket, { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port:8080})
+const wss = new WebSocketServer({ port: 8080 })
 
-interface msgs{
-    data?:string
-    type:"create"|"broadcast"|"join"|"leave"|"pong"
+interface msgs {
+    data?: string
+    type: "create" | "broadcast" | "join" | "leave" | "pong"
 }
-type id =string
-const rooms:Map<id,Set<WebSocket>> =new Map()
-const sockets:Map<WebSocket,id>=new Map()
+type id = string
+const rooms: Map<id, Set<WebSocket>> = new Map()
+const sockets: Map<WebSocket, id> = new Map()
 const heartbeat = new Map<WebSocket, number>();
 // todo to make sure client is present in only one room
 
-function leaveCurrentroom(a:WebSocket){
-    if(sockets.has(a)){
-        const roomid=sockets.get(a)
-        Leaveroom(a,roomid!)
+function leaveCurrentroom(a: WebSocket) {
+    if (sockets.has(a)) {
+        const roomid = sockets.get(a)
+        Leaveroom(a, roomid!)
     }
 
 }
 
-function roomcreation(a:WebSocket):string{
+function roomcreation(a: WebSocket): string {
     leaveCurrentroom(a)
-const id = crypto.randomUUID()
-rooms.set(id,new Set)
-rooms.get(id)?.add(a)
-sockets.set(a,id)
-return id
+    const id = crypto.randomUUID()
+    rooms.set(id, new Set)
+    rooms.get(id)?.add(a)
+    sockets.set(a, id)
+    return id
 }
 
-function Leaveroom(a:WebSocket,room:string){  
+function Leaveroom(a: WebSocket, room: string) {
 
-    if(sockets.has(a)){
-            rooms.get(room!)?.delete(a)
-            sockets.delete(a)
+    if (sockets.has(a)) {
+        rooms.get(room!)?.delete(a)
+        sockets.delete(a)
 
     }
 
-    if(rooms.get(room)?.size===0){
+    if (rooms.get(room)?.size === 0) {
         rooms.delete(room)
     }
 }
 
-wss.on("connection",(socket)=>{
-heartbeat.set(socket,Date.now())
-    socket.on("message",(Raw)=>{
-        
-        let msg:msgs
-        try{
+wss.on("connection", (socket) => {
+    heartbeat.set(socket, Date.now())
+    socket.on("message", (Raw) => {
 
-        msg=JSON.parse(Raw.toString())
+        let msg: msgs
+        try {
+
+            msg = JSON.parse(Raw.toString())
         }
-        catch(e){
+        catch (e) {
             return
         }
-        if (msg.type==="create"){
-            const id=roomcreation(socket)
-            socket.send("Room id "+id)
+        if (msg.type === "create") {
+            const id = roomcreation(socket)
+            socket.send("Room id " + id)
         }
-        
-        if (msg.type==="join"&&msg.data){
-            if(rooms.has(msg.data)){
+
+        if (msg.type === "join" && msg.data) {
+            if (rooms.has(msg.data)) {
                 leaveCurrentroom(socket)
                 rooms.get(msg.data)?.add(socket)
-                sockets.set(socket,msg.data)
+                sockets.set(socket, msg.data)
                 socket.send("Connected to room succesfully")
             }
         }
 
-        if (msg.type==="broadcast"&&msg.data){
-            if (sockets.has(socket)){
-                const roomid=sockets.get(socket)
-                const allembers=rooms.get(roomid!)
-                    for (const client of allembers!){
-                        if (client!==socket&&(client.readyState===WebSocket.OPEN)){
-                            client.send(msg.data)
+        if (msg.type === "broadcast" && msg.data) {
+            if (sockets.has(socket)) {
+                const roomid = sockets.get(socket)
+                const allembers = rooms.get(roomid!)
+                for (const client of allembers!) {
+                    if (client !== socket && (client.readyState === WebSocket.OPEN)) {
+                        client.send(msg.data)
                     }
                 }
                 socket.send("Succesfully deliverd")
             }
-            else{
+            else {
                 socket.send("Join the room first room")
             }
 
 
 
         }
-        if(msg.type==="leave"){
-            const room= sockets.get(socket)
-            if (room){
-                Leaveroom(socket,room)
+        if (msg.type === "leave") {
+            const room = sockets.get(socket)
+            if (room) {
+                Leaveroom(socket, room)
                 socket.send("Left the room")
                 heartbeat.delete(socket)
                 socket.close()
 
             }
-            
+
 
         }
-        if(msg.type==="pong"){
-             heartbeat.set(socket, Date.now());
+        if (msg.type === "pong") {
+            heartbeat.set(socket, Date.now());
         }
 
     })
-    socket.on("close",()=>{
+    socket.on("close", () => {
         leaveCurrentroom(socket)
         heartbeat.delete(socket)
     })
-    
+
 })
-setInterval(()=>{
+setInterval(() => {
     const now = Date.now()
-    for (const [ws,date] of heartbeat){
-        if(now-date>30000){
+    for (const [ws, date] of heartbeat) {
+        if (now - date > 30000) {
             heartbeat.delete(ws)
             leaveCurrentroom(ws)
-             continue;
+            continue;
         }
-        if (ws.readyState===WebSocket.OPEN){
+        if (ws.readyState === WebSocket.OPEN) {
             ws.ping();
         }
 
     }
 
-               
-    },10000)
+
+}, 10000)
 
